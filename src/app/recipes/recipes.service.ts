96609947b9ff58c78577ad490e236d397f10440f
theJames <james.newton@cognizant.com>
2020-01-19 18:36:40 -0600

    tutorial content

---
+++ src/app/recipes/recipes.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { Recipe } from './recipe.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipesService {
+
+  private recipes: Recipe[] = [
+    {
+      id: 'r1',
+      title: 'Schnitzel',
+      imageUrl: '../../assets/food1.jpeg',
+      ingredients: ['Fries', 'Pork', 'Salad']
+    },
+    {
+      id: 'r2',
+      title: 'Croisant',
+      imageUrl: '../../assets/food2.jpg',
+      ingredients: ['Bread', 'Butter', 'Milk']
+    },
+  ]
+
+  constructor() { }
+
+  public getAllRecipes() {
+    return [...this.recipes];
+  }
+
+  public getRecipe(recipeId: string) {
+    return {
+      ...this.recipes.find(recipe => {
+      return recipe.id === recipeId;
+    })
+  };
+  }
+}
